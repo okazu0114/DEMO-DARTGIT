480f9a5a2efe50a50b5a245af24ee92fd9cd1986
Tokkyer Midlow <mltokky@gmail.com>
2021-11-15 03:06:20 +0900

    add: assertのサンプルコード

--- src/control_flow.dart
+++ src/control_flow.dart
@@ -6,4 +6,9 @@ void main(List<String> args) {
   for (final index in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]) {
     print("index: $index");
   }
+
+  // assert (required to enable asserts option)
+  var a = 1;
+  assert(a == 2, "a isn't 2");
+  print("Finished!");
 }
